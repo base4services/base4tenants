0e294243adaf4a85d5bf6116d9a7b1728052a8fe
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2024-11-20 11:43:41 +0100

    n

--- src/services/tenants/models/__init__.py
+++ src/services/tenants/models/__init__.py
@@ -1,1 +1,7 @@
-from .generated_tenants_model import *
+import os
+from base4.utilities.common import import_all_from_dir
+import_all_from_dir(
+	directory=os.path.dirname(__file__),
+    package=__name__,
+    namespace=globals()
+)
--- src/services/tenants/services/__init__.py
+++ src/services/tenants/services/__init__.py
@@ -1,4 +1,7 @@
-from .keyauth import *
-from .options import *
-from .tenants import *
-from .users import *
+import os
+from base4.utilities.common import import_all_from_dir
+import_all_from_dir(
+	directory=os.path.dirname(__file__),
+    package=__name__,
+    namespace=globals()
+)
