315c0e867436d2d1c48f443da9659202126cd211
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2024-12-01 23:05:07 +0100

    n

--- tests/test_tenants.py
+++ tests/test_tenants.py
@@ -1,7 +1,14 @@
+import os
 
-from .test_base_tenants import TestBaseTenants
+current_file_path = os.path.abspath(os.path.dirname(__file__))
+from .test_base_tenants import TestBaseTenantsAPIV2
 
-class TestTenants(TestBaseTenants):
-
-    async def test(self):
-        assert True
+class TestSVC(TestBaseTenantsAPIV2):
+	services = ['tenants']
+	
+	async def setup(self):
+		await super().setup()
+	
+	async def test_is_tenants_healthy(self):
+		response = await self.request(method='get', url="/api/tenants/healthy")
+		assert response.status_code == 200
