9752bf6ae6d75542d7eb354c0d782e99bd246703
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2024-12-11 11:17:16 +0100

    n

--- tests/test_base_tenants.py
+++ tests/test_base_tenants.py
@@ -3,6 +3,7 @@ import datetime
 import ujson as json
 import os
 import pytest
+from typing import Dict
 import pprint
 import importlib
 from io import StringIO
@@ -13,22 +14,22 @@ from fastapi.testclient import TestClient
 from base4.utilities.service.startup import service as app
 import uuid
 import httpx
-
+from httpx import AsyncClient
 
 @pytest.mark.asyncio
 class TestBaseTenantsAPIV2:
-    app = app
     services = []
-    client = TestClient(app)
+    app: FastAPI = FastAPI()
+    default_tenant_code = "TEST"
     current_logged_user = None
-    default_tenant_code = 'TEST'
 
     async def setup(self):
-
+        self.get_app()
+        
         if 'tenants' not in self.services:
             self.services.append("tenants")
         
-        healthy_test = self.client.get('/api/tenants/healthy')
+        healthy_test = await self.request(method='get', url='/api/tenants/healthy')
         assert healthy_test.status_code == 200
 
         res = await self.request(method='post', url='/api/tenants/initialize',
@@ -48,22 +49,9 @@ class TestBaseTenantsAPIV2:
         assert res.status_code == 200
         assert 'token' in res.json()
         self.current_logged_user = {'username': 'admin', 'token': res.json()['token']}
-
-    @pytest.fixture(autouse=True, scope="function")
-    async def setup_fixture(self) -> None:
-        """
-        Fixture for tests of application.
-        It will be executed everytime before each test and after.
-
-        decorator:
-                @pytest.fixture(autouse=True, scope="function")
-                        autouse (bool): If true this fixture will be executed
-                                                        before and after every test.
-                        scope (str): This fixture is meant for function tests.
-
-        :return: None
-        """
         
+
+    def get_app(self):
         for service in self.services:
             try:
                 module = importlib.import_module(f'services.{service}.api.run')
@@ -71,25 +59,54 @@ class TestBaseTenantsAPIV2:
             except Exception as e:
                 raise
 
+    @pytest.fixture(autouse=True, scope="function")
+    async def setup_fixture(self) -> None:
         self.app.app_services = self.services
         await startup_event(self.services)
         await self.setup()
         yield
         await shutdown_event()
-        
+
     async def request(self, method: str, url: str, json_data: dict = None, data: dict = None, params=None, headers={}, files=[]):
-        #if 'Authorization' not in headers:
-        if self.current_logged_user and "token" in self.current_logged_user and self.current_logged_user["token"]:
-            headers['Authorization'] = f'Bearer {self.current_logged_user["token"]}'
-        
-        if json_data:
-           json_data = json.loads(json.dumps(json_data, default=str))
-           
-        response = self.client.request(
-            method=method, url=url, json=json_data, data=data, params=params, headers=headers,
-            cookies={'token': f'{self.current_logged_user["token"]}' if self.current_logged_user and "token" in self.current_logged_user else None, },
-            files=files
-        )
-        if response.status_code == 422:
-            return json.loads(response.text)
+
+        _method = method.lower()
+
+        params: Dict = {
+            'url': url,
+        }
+
+        if not headers:
+            headers = {}
+
+        params['headers'] = headers
+
+        if 'Authorization' not in headers:
+            if self.current_logged_user and "token" in self.current_logged_user and self.current_logged_user["token"]:
+                headers['Authorization'] = f'Bearer {self.current_logged_user["token"]}'
+
+        if params:
+            params['params'] = params
+
+        if _method not in ('delete', 'get'):
+
+            if json_data:
+                json_data = json.loads(json.dumps(json_data, default=str))
+
+            params['json'] = json_data if json_data else {}
+
+        async with httpx.AsyncClient(app=self.app, base_url='https://test') as client:
+            client.cookies.set(
+                'token',
+                f'{self.current_logged_user["token"]}' if self.current_logged_user and "token" in self.current_logged_user else None,
+            )
+            func = getattr(client, _method, None)
+
+            if not func:
+                raise Exception(f'Invalid method: {_method}')
+
+            try:
+                response = await func(**params)
+            except Exception as e:
+                raise
+
         return response
